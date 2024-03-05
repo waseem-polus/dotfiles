0053d6a0a89199dcac7f79f5887ca71c4d4a859d
waseem-polus <waseem_imad@yahoo.com>
2024-03-05 11:27:24 -0600

    added bash script to install pkgs based on OS

--- install-tools.sh
+++ install-tools.sh
@@ -1,3 +1,31 @@
+#!/bin/bash
 OS=$(uname)
 
-echo "You are using $OS"
+# Custom file containing package names and versions
+packages="packages.txt"
+
+# Install packages using apt (for Linux)
+apt_install() {
+    while read -r package; do
+        sudo apt install -y "$package"
+    done < "$packages"
+}
+
+# Install packages using brew (for MacOS)
+brew_install() {
+    while read -r package; do
+        brew install "$package"
+    done < "$packages"
+}
+
+# Check the operating system
+if [[ $(uname) == "Linux" ]]; then
+    echo "Installing for Linux using apt"
+    apt_install
+elif [[ $(uname) == "Darwin" ]]; then
+    echo "Installing for MacOS using brew"
+    brew_install
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
